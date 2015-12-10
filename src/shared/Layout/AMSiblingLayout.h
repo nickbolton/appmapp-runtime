e5919df6b9d12f564cc7a8e8151676c4bc23e5b2
Nick Bolton <nick.bolton@nike.com>
2015-12-09 17:20:59 -0800

    manual-constraints

---
+++ src/shared/Layout/AMSiblingLayout.h
@@ -0,0 +1,13 @@
+//
+//  AMSiblingLayout.h
+//  AppMap
+//
+//  Created by Bolton, Nicholas on 12/9/15.
+//  Copyright © 2015 Pixelbleed LLC. All rights reserved.
+//
+
+#import "AMLayout.h"
+
+@interface AMSiblingLayout : AMLayout
+
+@end
---
+++ src/shared/Layout/AMSiblingLayout.m
@@ -0,0 +1,13 @@
+//
+//  AMSiblingLayout.m
+//  AppMap
+//
+//  Created by Bolton, Nicholas on 12/9/15.
+//  Copyright © 2015 Pixelbleed LLC. All rights reserved.
+//
+
+#import "AMSiblingLayout.h"
+
+@implementation AMSiblingLayout
+
+@end
